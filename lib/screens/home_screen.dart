3d8aed1db09fa81db7c900d0c14ce003e7714fc6
akashrajput25 <55013687+akashrajput25@users.noreply.github.com>
2020-06-06 16:16:00 +0530

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,209 @@
+
+
+import 'package:covid19/constants.dart';
+import 'package:covid19/screens/details_scree.dart';
+import 'package:covid19/widgets/info_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+
+//wrap singlechildscrollview for correct displaying in small density devices
+      body: SingleChildScrollView(
+              child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:
+           <Widget>[
+            Container(
+              padding: EdgeInsets.only(left: 20, top: 20, right: 20, bottom: 20),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: kPrimaryColor.withOpacity(0.03),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50),
+                  bottomRight: Radius.circular(50),
+                ),
+              ),
+              child: Wrap(
+                runSpacing: 20,
+                spacing: 20,
+                children: <Widget>[
+                  InfoCard(
+                    title: "Total Cases",
+                    iconColor: Colors.blue,
+                    effectedNum: 1017,
+                    press: () {},
+                  ),
+                  InfoCard(
+                    title: "Total Deaths",
+                    iconColor: Colors.red,
+                    effectedNum: 7,
+                    press: () {},
+                  ),
+                  InfoCard(
+                    title: "Total Recovered",
+                    iconColor: Colors.green,
+                    effectedNum: 435,
+                    press: () {},
+                  ),
+                  InfoCard(
+                    title: "New Cases",
+                    iconColor: Colors.pink,
+                    effectedNum: 128,
+                    press: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return DetailsScreen();
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Stay home and Stay helathy🚴‍♀️" ,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+                    buildPreventation(),
+                    SizedBox(height: 40),
+                    buildHelpCard(context)
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row buildPreventation() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        PreventitonCard(
+          svgSrc: "assets/icons/hand_wash.svg",
+          title: "Wash Hands",
+        ),
+        PreventitonCard(
+          svgSrc: "assets/icons/use_mask.svg",
+          title: "Use Masks",
+        ),
+        PreventitonCard(
+          svgSrc: "assets/icons/clean_disinfect.svg",
+          title: "Clean things regularly",
+        ),
+      ],
+    );
+  }
+
+  Container buildHelpCard(BuildContext context) {
+    return Container(
+      height: 150,
+      width: double.infinity,
+      child: Stack(
+        alignment: Alignment.bottomLeft,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              // left side padding is 40% of total width
+              left: MediaQuery.of(context).size.width * .4,
+              top: 20,
+              right: 20,
+            ),
+            height: 130,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.red,
+                  Colors.red[200],
+                ],
+              ),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Dial 104 for \nany COVID-19 assistance!\n",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(color: Colors.white),
+                  ),
+                  TextSpan(
+                    text: "*For Jharkhand citizens",
+                    style: TextStyle(
+                      color: Colors.green.withOpacity(0.7),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: SvgPicture.asset("assets/icons/nurse.svg"),
+          ),
+          Positioned(
+            top: 30,
+            right: 10,
+            child: SvgPicture.asset("assets/icons/virus.svg"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: kPrimaryColor.withOpacity(.03),
+      elevation: 0,
+      title: Text('  JH COVID 19 Tracker', textAlign: TextAlign.center, style:TextStyle(fontWeight:FontWeight.bold,fontSize:35,color:Colors.white,))
+    );
+  }
+}
+
+class PreventitonCard extends StatelessWidget {
+  final String svgSrc;
+  final String title;
+  const PreventitonCard({
+    Key key,
+    this.svgSrc,
+    this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SvgPicture.asset(svgSrc),
+        Text(
+          title,
+          style:
+              Theme.of(context).textTheme.bodyText1.copyWith(color: kPrimaryColor),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/widgets/info_card.dart
@@ -0,0 +1,108 @@
+import 'package:covid19/constants.dart';
+import 'package:covid19/widgets/line_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class InfoCard extends StatelessWidget {
+  final String title;
+  final int effectedNum;
+  final Color iconColor;
+  final Function press;
+  const InfoCard({
+    Key key,
+    this.title,
+    this.effectedNum,
+    this.iconColor,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return GestureDetector(
+          onTap: press,
+          child: Container(
+            width: constraints.maxWidth / 2 - 10,
+            // Here constraints.maxWidth provide us the available width for the widget
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: SingleChildScrollView(
+                          child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget>[
+                        // wrapped within an expanded widget to allow for small density device
+                        Expanded(
+                            child: Container(
+                            alignment: Alignment.center,
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                              color: iconColor.withOpacity(0.22),
+                              shape: BoxShape.circle,
+                            ),
+                            child: SvgPicture.asset(
+                              "assets/icons/running.svg",
+                              height: 12,
+                              width: 12,
+                              color: iconColor,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 5),
+                        Text(
+                          title,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: RichText(
+                            text: TextSpan(
+                              style: TextStyle(color: kTextColor),
+                              children: [
+                                TextSpan(
+                                  text: "$effectedNum \n",
+                                  style:
+                                      Theme.of(context).textTheme.headline6.copyWith(
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                ),
+                                TextSpan(
+                                  text: "People",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    height: 2,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: LineReportChart(),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
